b0089dddb76c28019ee0cb6edcc986127f1b531e
Mike Lowe <mikelowedev@gmail.com>
2019-04-07 13:30:53 +0100

    Create docker image manually

--- deployment/create.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-WORKING_DIR=$(dirname "$0")
-
-kubectl create -f $WORKING_DIR/cluster-role-binding.yaml
-kubectl create -f $WORKING_DIR/service.yaml
-kubectl create -f $WORKING_DIR/deployment.yaml
